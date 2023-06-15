4382f388091cc008fc61a26c0baba78e95f2aaca
SySyAli <syedansabali786@gmail.com>
2023-06-15 01:15:18 -0400

    creating basic server using fastify

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('Hello World')
+import fastify from 'fastify';
+
+const PORT = process.env.PORT || 3000;
+
+const app = fastify({ logger: true });
+
+app.get('/', async (request, reply) => {
+  return { hello: 'world' };
+});
+
+// figure out how to use PORT (error from process.env.PORT)
+app.listen({ port: 3000 }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});
