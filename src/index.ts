bf7187b8a11482057e914f325c0f861a13fe9f65
SySyAli <syedansabali786@gmail.com>
2023-06-17 14:37:17 -0400

    adding meilisearch to api

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,37 @@ import mercurius from 'mercurius';
 import db from './config/index';
 import schema from './graphql/schema';
 import resolvers from './graphql/resolver';
+import { MeiliSearch } from 'meilisearch';
+import courseCatalog from './models/courseCatalog';
 
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODBI_URI || 'mongodb://localhost:27017';
 
 const app = fastify({ logger: true });
 
-app.register(db, {uri: 'mongodb://localhost:27017'})
-app.register(mercurius, { schema, resolvers, graphiql: true })
+// setting MeiliSearch client
+const client = new MeiliSearch({ host: 'http://localhost:7700' });
+const addCoursesToIndex = async () => {
+  const courses = await courseCatalog.find({});
+  const res = await client.index('courses').addDocuments(courses);
+  console.log(res);
+};
+addCoursesToIndex();
+client.getTask(0);
+
+app.register(db, { uri: 'mongodb://localhost:27017' });
+app.register(mercurius, { schema, resolvers, graphiql: true });
 
 app.get('/', async (request, reply) => {
   return { hello: 'world' };
 });
-
+/*
+app.get('/search', async (request, reply) => {
+  const client = new MeiliSearch({ host: 'http://localhost:7700' });
+  const res = await client.index('courses').search(request.query.q);
+  return res;
+}
+*/
 // figure out how to use PORT (error from process.env.PORT)
 app.listen({ port: 3000 }, (err, address) => {
   if (err) {
